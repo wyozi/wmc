c433eccfd655467fcb173c4072691e79d07adc3e
wyozi <joonasmertanen@gmail.com>
2013-11-30 18:03:46 +0200

    Disable db storage by default

--- lua/sv_wmc_dbconfig.lua
+++ lua/sv_wmc_dbconfig.lua
@@ -3,7 +3,7 @@
 -- Database integration requires FruityDB SQL abstraction plugin (https://github.com/wyozi/fruitydb/)
 -- It can be downloaded from https://github.com/wyozi/fruitydb/archive/master.zip and should be unzipped to addons folder
 
-wyozimc.UseDatabaseStorage = true
+wyozimc.UseDatabaseStorage = false
 wyozimc.DatabaseDetails = {
 	Host = "localhost",
 	User = "root",
